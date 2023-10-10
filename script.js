6b508dda81790f2b0dd44a728227e1904ef98d1b
Umar12Rindkh <147235222+Umar12Rindkh@users.noreply.github.com>
2023-10-10 06:19:20 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const rindi = document.querySelector('.regist');
+const umar = document.querySelector('.dropdown');
+
+rindi.addEventListener('click', function(){
+    umar.classList.toggle('fix');
+});
