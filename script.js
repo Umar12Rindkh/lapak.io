f65e0683f96ec98495b383ed40e133f0f93bfacf
Umar12Rindkh <147235222+Umar12Rindkh@users.noreply.github.com>
2023-10-12 09:43:35 -0700

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const umar = document.querySelector('.dropdown');
 rindi.addEventListener('click', function(){
     umar.classList.toggle('fix');
 });
+
+const menutoggle = document.querySelector('.menubtn input');
+const nav = document.querySelector('.navbar');
+const fix = document.querySelector('body');
+
+menutoggle.addEventListener('click' , function(){
+    nav.classList.toggle('slide');
+    fix.classList.toggle('fix');
+})
+
